04b15774245fef1f6a6cc947dd8b2cabd6a41e94
Shreyaskr1409 <shreyaskr.0914@gmail.com>
2025-04-11 15:16:33 +0530

    Initialized server

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github/abhishekkujur1/SmartClassroom
+module github.com/abhishekkujur1/SmartClassroom
 
 go 1.23.6
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/abhishekkujur1/SmartClassroom/server"
+
+func main() {
+	server.StartServer()
+}
